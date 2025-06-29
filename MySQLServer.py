b01975589f3d6681bd40b518ccd8a36db5ddfcd6
Alvin karani <alvinkaranimudembei@gmail.com>
2025-06-29 17:25:58 +0300

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -16,6 +16,7 @@ def create_database():
             cursor = connection.cursor()
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
+            "except mysql.connector.Error"
 
     except Error as e:
         print(f"Error: {e}")
